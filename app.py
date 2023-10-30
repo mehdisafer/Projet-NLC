59d9b270736fa609bae95dc4498b32cee9543eb8
mehdisafer <mehdisaferpro@gmail.com>
2023-10-30 15:57:07 +0100

    problem

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from components.api import ai
 
 st.title("Générateur de code :male-technologist:")
 
@@ -18,7 +19,31 @@ sélection = st.selectbox("Sélectionnez une langue", ["Python", "JavaScript", "
 description_code = st.text_area("Quelle Code souhaitez-vous généré", height=200)
 Buton_1 = st.button('Générer')
 
-generation_code = st.text_area("code généré par L'API OpenIA", height=200)
+# Appel de la fonction API
+def générer_code(description, langue):
+    return ai(description + " - " + langue)
+
+# Code généré
+code_généré = générer_code(description_code, sélection)
+
+
+generation_code = st.text_area("code généré par L'API OpenIA", code_généré, height=200)
+#Buton_2= st.button("exécuter")
+
+# exécution_code = st.text_area("Exécution du code généré par L'API OpenIA",exécution_code, height=200)
+
+# Exécution du code
+def exécuter_code(code):
+    try:
+        code_exec = exec(code)
+    except Exception as e:
+        st.write(e)
+    return code_exec
+
+# Bouton d'exécution
 Buton_2= st.button("exécuter")
 
-exécution_code = st.text_area("Exécution du code généré par L'API OpenIA", height=200)
+if Buton_2 == True:
+    exécution_code = exécuter_code(code_généré)
+    value = exécution_code
+    st.text_area("Exécution du code généré par L'API OpenIA,",value, height=200)
